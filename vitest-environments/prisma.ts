6d191414673e6932e16e3df4a39703d527183be0
Junior <ismaeljunior1508@gmail.com>
2024-02-20 17:47:30 -0300

    feat: enviroments for tests E2Econfigurate

--- vitest-environments/prisma.ts
+++ vitest-environments/prisma.ts
@@ -28,6 +28,7 @@ export default <Environment>{
 		return {
 			async teardown() {
 				await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+				await prisma.$disconnect;
 			},
 		};
 	},
