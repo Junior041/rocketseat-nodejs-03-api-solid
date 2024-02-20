e8541bfce3ebaa2cb76f8170231f720dc06560a8
Junior <ismaeljunior1508@gmail.com>
2024-02-20 16:55:45 -0300

    feat: add controler for return profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,19 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { makeGetUserProfileUseCase } from "../../use-cases/factories/make-get-uer-profile-use-case";
 
 export async  function profile(request: FastifyRequest, reply: FastifyReply){
 	await request.jwtVerify();
-	console.log(request.user.sub);
-	
-	return reply.status(201).send();
+	const getUserProfile = makeGetUserProfileUseCase();
+
+	const {user} = await getUserProfile.execute({
+		userId: request.user.sub
+	});
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const { password_hash, ...userWithoutPassword } = user;
+
+
+	return reply.status(200).send({
+		user: userWithoutPassword
+	});
 }
