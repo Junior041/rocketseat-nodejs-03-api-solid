053fd9b46ff641b2298488726ece14d78f7c81af
Junior <ismaeljunior1508@gmail.com>
2024-02-21 16:36:58 -0300

    feat: check-ins controller

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -2,21 +2,31 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makeCheckInUseCase } from "../../../use-cases/factories/make-check-in-use-case";
 
-export async  function create(request: FastifyRequest, reply: FastifyReply){
+export async function create(request: FastifyRequest, reply: FastifyReply) {
 	const createCheckInParamsSchema = z.object({
-		gymId: z.string().uuid()
+		gymId: z.string().uuid(),
 	});
+
 	const createCheckInBodySchema = z.object({
-		latitude: z.number().refine(value => {
+		latitude: z.number().refine((value) => {
 			return Math.abs(value) <= 90;
 		}),
-		longitude: z.number().refine(value => {
-			return Math.abs(value) <= 100;
+		longitude: z.number().refine((value) => {
+			return Math.abs(value) <= 180;
 		}),
 	});
-	const { gymId }  = createCheckInParamsSchema.parse(request.body);
+
+	const { gymId } = createCheckInParamsSchema.parse(request.params);
 	const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
-	const createCheckInUseCase = makeCheckInUseCase();
-	await createCheckInUseCase.execute({gymId, userLatitude: latitude, userLongitude: longitude, userId: request.user.sub});
+
+	const checkInUseCase = makeCheckInUseCase();
+
+	await checkInUseCase.execute({
+		gymId,
+		userId: request.user.sub,
+		userLatitude: latitude,
+		userLongitude: longitude,
+	});
+
 	return reply.status(201).send();
 }
--- src/http/controllers/check-ins/history.ts
+++ src/http/controllers/check-ins/history.ts
@@ -2,15 +2,21 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makeFetchUserCheckInsHistoryUseCase } from "../../../use-cases/factories/make-fecth-user-check-ins-history-use-case";
 
-export async  function history(request: FastifyRequest, reply: FastifyReply){
-    
+export async function history(request: FastifyRequest, reply: FastifyReply) {
 	const checkInHistoryQuerySchema = z.object({
-		q: z.string(),
-		page: z.coerce.number().min(1).default(1)
+		page: z.coerce.number().min(1).default(1),
 	});
 
 	const { page } = checkInHistoryQuerySchema.parse(request.query);
-	const fetchUserCheckInHistoryUseCase = makeFetchUserCheckInsHistoryUseCase();
-	const { checkIns } = await fetchUserCheckInHistoryUseCase.execute({ page, userId: request.user.sub });
-	return reply.status(201).send({checkIns});
+
+	const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase();
+
+	const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
+		page,
+		userId: request.user.sub,
+	});
+
+	return reply.status(200).send({
+		checkIns,
+	});
 }
