0a63b0c811260db83b66eb4b18095bdc79abfdcc
Junior <ismaeljunior1508@gmail.com>
2024-02-21 15:30:31 -0300

    feat: added validate check-in controller

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,15 @@
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+	const validateCheckInParamsSchema = z.object({
+		checkInId: z.string().uuid(),
+	});
+  
+	const { checkInId } = validateCheckInParamsSchema.parse(request.params);
+  
+	const validateCheckInUseCase = makeValidateCheckInUseCase();
+  
+	await validateCheckInUseCase.execute({
+		checkInId,
+	});
+  
+	return reply.status(204).send();
+}
