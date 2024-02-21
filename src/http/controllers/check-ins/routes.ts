2ec9118f801daec3f38fed07db2713822562c49e
Junior <ismaeljunior1508@gmail.com>
2024-02-21 15:17:41 -0300

    feat: added check-in routes

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,14 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
-import { search } from "./search";
-import { nearby } from "./nearby";
-import { create } from "./register";
 
-export async function gymsRoutes(app: FastifyInstance) {
+export async function checkInsRoutes(app: FastifyInstance) {
 	app.addHook("onRequest", verifyJWT);
 
-	app.get("/gyms/search", search);
-	app.get("/gyms/nearby", nearby);
-
-	app.post("/gyms", create);
 }
