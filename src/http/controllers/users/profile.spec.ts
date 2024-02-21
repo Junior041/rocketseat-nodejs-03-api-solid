4a719412928a29980b42902aa19848574e1cf58f
Junior <ismaeljunior1508@gmail.com>
2024-02-21 16:02:19 -0300

    reafactor: create a function for creat  and authenticate user for tests

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import request from "supertest";
 import { app } from "../../../app";
+import { createAndAuthenticateUser } from "../../../utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", () => {
 	beforeAll(async() => {
@@ -10,22 +11,8 @@ describe("Profile (e2e)", () => {
 		await app.close();
 	});
 	it("should be able to get user profile", async () => {
-		await request(app.server)
-			.post("/users")
-			.send({
-				name: "John Doe",
-				email: "johndoe@exemple.com",
-				password: "123456"
-			});
 
-		const authResponse = await request(app.server)
-			.post("/sessions")
-			.send({
-				email: "johndoe@exemple.com",
-				password: "123456"
-			});
-
-		const { token } = authResponse.body;
+		const { token } = await createAndAuthenticateUser(app);
 
 		const profileResponse = await request(app.server)
 			.get("/me")
