b356903a5130a576c524404bcfe50dc838a6e10b
Junior <ismaeljunior1508@gmail.com>
2024-02-21 15:15:36 -0300

    refactor: refactorade name function

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makeFetchNearbyGymsUseCase } from "../../../use-cases/factories/make-fecth-nearby-gyms-use-case";
 
-export async function feachNearbyGyms(request: FastifyRequest, reply: FastifyReply){
+export async function nearby(request: FastifyRequest, reply: FastifyReply){
     
 	const nearbyGymsQuerySchema = z.object({
 		latitude: z.number().refine(value => {
