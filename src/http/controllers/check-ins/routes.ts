2f0103a99099c326839d87b548e360dd346cae6e
Junior <ismaeljunior1508@gmail.com>
2024-02-21 15:31:11 -0300

    feat: check-in routes

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,7 +3,11 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { create } from "./create";
 
 export async function checkInsRoutes(app: FastifyInstance) {
-	app.addHook("onRequest", verifyJWT);
-
+	app.addHook("onRequest", verifyJwt);
+  
+	app.get("/check-ins/history", history);
+	app.get("/check-ins/metrics", metrics);
+  
 	app.post("/gyms/:gymId/check-ins", create);
+	app.patch("/check-ins/:checkInId/validate", validate);
 }
